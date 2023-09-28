02504844c73c208e8c4f31d58b5d0322d8e15c56
Jo Young <126527025+gitjoyoung@users.noreply.github.com>
2023-09-29 01:38:31 +0900

    [unrated] Title: 문자 개수 세기, Time: 0.04 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/unrated/181902. 문자 개수 세기/문자 개수 세기.js
+++ 프로그래머스/unrated/181902. 문자 개수 세기/문자 개수 세기.js
@@ -1,16 +1,9 @@
+
 function solution(my_string) {
-    const arr = Array(52).fill(0)
-    return [...my_string].reduce((acc, cur) => {
-        let curIdx
-        const curAlpha= cur.charCodeAt()
-        if(curAlpha >= 97) {
-         curIdx = curAlpha - 71   
-        } else {
-         curIdx = curAlpha-65   
-        }
-         
-        acc[curIdx] = acc[curIdx] ?? 0
-        acc[curIdx]++
-        return acc
-    }, arr)
+    let alp = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz';
+    var answer = new Array(52).fill(0);
+    for (let i = 0; i < my_string.length; i++) {
+        answer[alp.indexOf(my_string[i])]++;
+    }
+    return answer;
 }
